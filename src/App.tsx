29cb8a1a4ca5d9c8affe13d30ee3ce77529a4ac9
nelsonoftrafalgar <nelsonoftrafalgar@gmail.com>
2019-05-16 22:02:14 +0200

    add some inputs and handlers

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,24 @@ import { List } from './containers/List/List'
 import { data } from './db'
 import styles from './App.module.css'
 
+export interface ISketch {
+  name: string
+  checked: boolean
+}
+
 const App: React.FC = () => {
 
   const [addedSketches, setAddedSketches] = useState<number>(0)
-  const [sketches, setSketches] = useState<string[]>([])
+  const [sketches, setSketches] = useState<ISketch[]>([])
 
   const random = Math.floor(Math.random() * data.length)
 
   const handleAddSketch = () => {
-    setSketches([...sketches, data[random]])
+    const newSketch = {
+      name: data[random],
+      checked: false
+    }
+    setSketches([...sketches, newSketch])
     setAddedSketches(addedSketches + 1)
   }
 
@@ -23,6 +32,27 @@ const App: React.FC = () => {
 
   const handleClearList = () => {
     setSketches([])
+  }
+
+  const handleCheck = (e: React.FormEvent<HTMLInputElement>) => {
+    const name = e.currentTarget.value
+    const updatedList = sketches.map((sketch) => {
+      if (sketch.name === name) {
+        return {
+          ...sketch,
+          checked: !sketch.checked
+        }
+      }
+      return sketch
+    })
+
+    setSketches(updatedList)
+  }
+
+  const handleRemove = (name: string) => (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+    const updatedList = sketches.filter((sketch) => sketch.name !== name)
+    setSketches(updatedList)
   }
 
   const currentSketches = sketches.length
@@ -36,7 +66,11 @@ const App: React.FC = () => {
         addedSketches={addedSketches}
         currentSketches={currentSketches}
       />
-      <List sketches={sketches} />
+      <List
+        sketches={sketches}
+        handleCheck={handleCheck}
+        handleRemove={handleRemove}
+      />
     </div>
   )
 }
--- src/containers/List/List.tsx
+++ src/containers/List/List.tsx
@@ -1,13 +1,26 @@
+import { ISketch } from '../../App'
 import React from 'react'
 import styles from './List.module.css'
+
 interface IListProps {
-  sketches: string[]
+  sketches: ISketch[]
+  handleCheck: (e: React.FormEvent<HTMLInputElement>) => void
+  handleRemove: (name: string) => (e: React.MouseEvent<HTMLButtonElement>) => void
 }
 
-const List: React.FC<IListProps> = ({ sketches }) => {
+const List: React.FC<IListProps> = ({ sketches, handleCheck, handleRemove }) => {
 
-  const sketchList = sketches.map((sketch: string) => {
-    return <li key={sketch}>{sketch}</li>
+  const sketchList = sketches.map((sketch: ISketch) => {
+    const { name, checked } = sketch
+    return (
+      <li className={styles.listItem} key={name}>
+        <label>
+          <input type='checkbox' value={name} checked={checked} onChange={handleCheck} />
+          {name}
+        </label>
+        <button onClick={handleRemove(name)}>Remove</button>
+      </li>
+    )
   })
 
   return (
